5b2625adc0e4d54f24f62e5322f4f213148cbbe6
Ramazan Sürücü <36728918+Rmznsrc@users.noreply.github.com>
2024-10-29 13:24:01 +0300

    CQRS Design Pattern

---
+++ CQRSDesignPattern/DesignPattern.CQRS/Migrations/20241029071219_newmigcqrs.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DesignPattern.CQRS.Migrations
+{
+    public partial class newmigcqrs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Produts",
+                table: "Produts");
+
+            migrationBuilder.RenameTable(
+                name: "Produts",
+                newName: "Products");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "ProductID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "Produts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Produts",
+                table: "Produts",
+                column: "ProductID");
+        }
+    }
+}
