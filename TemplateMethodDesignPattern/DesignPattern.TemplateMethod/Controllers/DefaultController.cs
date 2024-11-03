349e69fe9f2c5229a37d1c76d17e14375d456d47
Ramazan Sürücü <36728918+Rmznsrc@users.noreply.github.com>
2024-11-03 14:49:08 +0300

    Completed - Template Method Design Pattern - Netflix Plans Example Finished

--- TemplateMethodDesignPattern/DesignPattern.TemplateMethod/Controllers/DefaultController.cs
+++ TemplateMethodDesignPattern/DesignPattern.TemplateMethod/Controllers/DefaultController.cs
@@ -16,5 +16,27 @@ namespace DesignPattern.TemplateMethod.Controllers
             //netflixPlans.CreatePlan();
             return View();
         }
+        public IActionResult StandardPlanIndex()
+        {
+            NetflixPlans netflixPlans = new StandardPlan();
+            ViewBag.v1 = netflixPlans.PlanType("Temel Plan");
+            ViewBag.v2 = netflixPlans.CountPerson(2);
+            ViewBag.v3 = netflixPlans.Price(150.00);
+            ViewBag.v4 = netflixPlans.Content("Film-Dizi-Animasyon");
+            ViewBag.v5 = netflixPlans.Resolution("720px");
+            //netflixPlans.CreatePlan();
+            return View();
+        }
+        public IActionResult UltraPlanIndex()
+        {
+            NetflixPlans netflixPlans = new UltraPlan();
+            ViewBag.v1 = netflixPlans.PlanType("Ultra Plan");
+            ViewBag.v2 = netflixPlans.CountPerson(4);
+            ViewBag.v3 = netflixPlans.Price(199.99);
+            ViewBag.v4 = netflixPlans.Content("Film-Dizi-Animasyon-Belgesel");
+            ViewBag.v5 = netflixPlans.Resolution("1080px");
+            //netflixPlans.CreatePlan();
+            return View();
+        }
     }
 }
