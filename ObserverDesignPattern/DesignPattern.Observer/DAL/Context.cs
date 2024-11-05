9d6d2781fb893a126e87004c72f3fc5c4779bf4d
Ramazan Sürücü <36728918+Rmznsrc@users.noreply.github.com>
2024-11-05 23:30:12 +0300

    Observer Design Pattern Example - Packages, Entities, Migration, Identity

---
+++ ObserverDesignPattern/DesignPattern.Observer/DAL/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Observer.DAL
+{
+    public class Context : IdentityDbContext<AppUser, AppRole, int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-VEO0HFH\\SQLEXPRESS;initial catalog=ObserverDesignPatternDB;integrated security=true;TrustServerCertificate=True;");
+        }
+        public DbSet<WelcomeMessage> WelcomeMessages { get; set; }  
+        public DbSet<Discount> Discounts { get; set; }  
+        public DbSet<UserProcess> UserProcesses { get; set; }  
+    }
+}
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 In this project, I will share 15 different design patterns with examples.
 Properties of design patterns,
 - Chain Of Responsibility Design Pattern: Code First, Migration, MSSQL, Core 8
-- CQRS Design Pattern: Code First, Migration, MSSQL, Core 3.1
+- CQRS Design Pattern: Code First, Migration, MSSQL, Core 3
+- Template Method Design Pattern: Code First, Migration, Core 5 
+- Observer Design Pattern: Code First, Migration, MSSQL, Core 5
