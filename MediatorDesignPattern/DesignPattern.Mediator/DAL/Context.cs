4cfbe3146a153089132b7cedb92fffb2c2848730
Ramazan Sürücü <36728918+Rmznsrc@users.noreply.github.com>
2024-11-17 10:28:13 +0300

    Mediator Design Pattern - Result, Query, Handler

---
+++ MediatorDesignPattern/DesignPattern.Mediator/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Mediator.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-VEO0HFH\\SQLEXPRESS;initial catalog=MediatorDB;integrated security=true;TrustServerCertificate=True;");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
