3070838e79142baf564d1d867448f52b3ab349f5
Ramazan Sürücü <36728918+Rmznsrc@users.noreply.github.com>
2024-11-21 23:54:13 +0300

    Facade Design Pattern - Order Facade Class

---
+++ FacadeDesignPattern/DesignPattern.Facade/FacadePattern/OrderFacade.cs
@@ -0,0 +1,29 @@
+﻿using DesignPattern.Facade.DAL;
+
+namespace DesignPattern.Facade.FacadePattern
+{
+    public class OrderFacade
+    {
+        Order order = new Order();
+        OrderDetail orderDetail = new OrderDetail();
+        ProductStock productStock = new ProductStock();
+
+        AddOrder addOrder = new AddOrder();
+        AddOrderDetail addOrderDetail = new AddOrderDetail();
+
+        public void CompleteOrder(int customerID, int productID, int orderID, int productCount, decimal productPrice, decimal totalProductPrice)
+        {
+            order.CustomerID = customerID;
+            addOrder.AddNewOrder(order);
+
+            orderDetail.CustomerID = customerID;
+            orderDetail.ProductID = productID;
+            orderDetail.ProductPrice = productPrice;
+            orderDetail.ProductCount = productCount;
+            totalProductPrice = productCount * productPrice;
+            orderDetail.ProductTotalPrice = totalProductPrice;
+            addOrderDetail.AddNewOrderDetail(orderDetail);
+        }
+
+    }
+}
