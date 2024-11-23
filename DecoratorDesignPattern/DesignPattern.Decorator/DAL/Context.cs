33c880091d40e007fc7fb50036f7706561919a48
Ramazan Sürücü <36728918+Rmznsrc@users.noreply.github.com>
2024-11-23 17:54:18 +0300

    Decorator Design Pattern - Notifier Class

--- DecoratorDesignPattern/DesignPattern.Decorator/DAL/Context.cs
+++ DecoratorDesignPattern/DesignPattern.Decorator/DAL/Context.cs
@@ -9,5 +9,6 @@ namespace DesignPattern.Decorator.DAL
             optionsBuilder.UseSqlServer("server=DESKTOP-VEO0HFH\\SQLEXPRESS;initial catalog=DecoratorPatternDB;integrated security=true;TrustServerCertificate=True;");
         }
         public DbSet<Message> Messages { get; set; }
+        public DbSet<Notifier> Notifiers { get; set; }
     }
 }
